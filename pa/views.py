0892e13aeea9fcb53ccd3c648768ded394d905cd
Chayapan Khannabha <chayapan@gmail.com>
2018-12-14 17:37:11 +0700

    Add/remove item from PADocument. Export to JSON.

--- pa/views.py
+++ pa/views.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response
 from django.views.generic import TemplateView
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseServerError, HttpResponse
 from django.db.models import Sum
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
-from django.http import HttpResponseServerError
 from decimal import Decimal
+import json
 import os, os.path
 
 from pa_items import PAItem, PAForm, WORKBOOK1
@@ -66,7 +66,7 @@ class NewDocument(TemplateView):
         data['programs'] = programs
         return render(request, self.template_name, data)
     def post(self, request):
-        print request.POST
+        # print request.POST
         support_staff_id = request.POST.get('support-staff')
         academic_staff_id = request.POST.get('academic-staff')
         fiscal_year = request.POST.get('fiscal-year')
@@ -75,8 +75,13 @@ class NewDocument(TemplateView):
         if os.path.exists(pa_file):
             return HttpResponseServerError("Active Document Already Exists")
         else:
+            academic_staff = AcademicStaff.loadFromWorkbook()[academic_staff_id]
+            support_staff = SupportStaff.loadFromWorkbook()[support_staff_id]
+            initial_data = {}
+            initial_data["academic_staff"] = {"fist_name": academic_staff.first_name , "last_name": academic_staff.last_name}
+            initial_data["support_staff"] = {"fist_name": support_staff.first_name , "last_name": support_staff.last_name}
             with open(pa_file,"w") as f:
-                f.write("{}") # Touch open file.
+                f.write(json.dumps(initial_data)) # Touch open file.
         print pa_file
         data = {}
         return HttpResponseRedirect(reverse("fill-document",kwargs={"year": fiscal_year,"staff_id": academic_staff_id}))
@@ -85,20 +90,14 @@ class FillDocument(TemplateView):
     """Display HTML page for creating new Performance Agreement."""
     template_name = "fill-document.html"
     def get(self, request, year, staff_id):
-        academic_staff_id = staff_id
-        fiscal_year = year
-        padoc_filename = """%s_%s.json""" % (fiscal_year, academic_staff_id)
-        pa_file = os.path.join(settings.DOCUMENT_FOLDER, padoc_filename)
-        if not os.path.exists(pa_file):
+        document = self.get_document(year, staff_id)
+        if not document:
             return HttpResponseServerError("Document Does Not Exists")
-        
-        # Load document file from JSON
-        document = PADocument.fromFile(pa_file)
         
         # Load data from database
         staffs = AcademicStaff.loadFromWorkbook() 
-        staff = staffs[academic_staff_id]
-        data = {"year": fiscal_year,"staff_id": academic_staff_id}
+        staff = staffs[staff_id]
+        data = {"year": year,"staff_id": staff_id}
         data['academic_staff'] = staff
         data['document'] = document
 
@@ -107,6 +106,42 @@ class FillDocument(TemplateView):
         data["items"] = PAForm(pa_items).view_items
 
         return render(request, self.template_name, data)
+    def post(self, request, year, staff_id):
+        document = self.get_document(year, staff_id)
+        if not document:
+            return HttpResponseServerError("Document Does Not Exists")
+        
+        # Adding
+        if "add-item" in request.POST:
+            pa_id = request.POST.get("pa-id")
+            pa_desc_th = request.POST.get("pa-desc-th")
+            pa_desc_en = request.POST.get("pa-desc-en")
+            pa_score = request.POST.get("pa-score")
+            detail = request.POST.get("pa-detail")
+            print document.lines
+            document.add_item(pa_id, pa_desc_th, pa_desc_en, detail, pa_score)
+            document.save() # Write JSON
+        # Removing Line
+        if "remove-line" in request.POST:
+            line_number = request.POST.get("line_number")
+            document.remove_line(line_number)
+            document.save()
+        if "download-document" in request.POST:
+            return HttpResponse(document.asJSON(), content_type="application/json")
+            
+        # Refresh
+        return HttpResponseRedirect(reverse("fill-document",kwargs={"year": year,"staff_id": staff_id}))
+    
+    def get_document(self, year, staff_id):
+        academic_staff_id = staff_id
+        fiscal_year = year
+        padoc_filename = """%s_%s.json""" % (fiscal_year, academic_staff_id)
+        pa_file = os.path.join(settings.DOCUMENT_FOLDER, padoc_filename)
+        if not os.path.exists(pa_file):
+            return False
+        # Load document file from JSON
+        document = PADocument.fromFile(pa_file)
+        return document
 
 class DocumentList(TemplateView):
     template_name = "list-documents.html"
--- pa_document.py
+++ pa_document.py
@@ -9,14 +9,55 @@ import os
 
 class PADocument:
     def __init__(self, filename, json_data):
-        self.filename = os.path.split(filename)[1]
+        self.folder, self.filename = os.path.split(filename)
         self._json = json_data
+        # Construct the line items
+        if "lines" in json_data:
+            self.lines = json_data["lines"]
+        else:
+            self.lines = {}
     @property
     def year(self):
         return self.filename.split("_")[0]
     @property
     def staff_id(self):
         return self.filename.split("_")[1][:-len(".json")]
+    @property
+    def next_line_number(self):
+        line_number = len(self.lines) + 1 # line number for this item
+        return line_number
+    
+    def add_item(self, pa_id, pa_desc_th, pa_desc_en, detail, score):
+        print "Adding", pa_id, pa_desc_th, pa_desc_en, detail, score
+        line_number = self.next_line_number
+        # Add to instance
+        self.lines[line_number] = {"id": pa_id, "desc_th": pa_desc_th, "desc_en": pa_desc_en, "detail": detail, "score": score}
+        # Add to store
+        self._json["lines"] = self.lines
+        return True
+    def remove_line(self, line_number):
+        new_lines = {}
+        c = 1
+        for k, line in self.lines.items():
+            if not int(line_number) == int(k): # Both are integer
+                new_lines[c] = line
+                c += 1
+        self.lines = new_lines
+        # Add to store
+        self._json["lines"] = self.lines
+        print "Removing", line_number
+        return True
+    def save(self):
+        pa_file = os.path.join(self.folder, self.filename)
+        with open(pa_file, "w") as f:
+            json_data = json.dumps(self._json, sort_keys=True, indent=4)
+            f.write(json_data)
+            # print json_data
+            print self.filename
+        return True
+    def asJSON(self):
+        json_data = json.dumps(self._json, sort_keys=True, indent=4)
+        return json_data
     @classmethod 
     def fromFile(cls, filename):
         with open(filename, "r") as f:
